9dcc8943b7a6ee09aebc0ffafccaa85cbed2de01
Michael E Brown <michael_e_brown@dell.com>
2011-09-27 10:39:59 -0500

    licensing fixups

--- lcctool/plugins/raccfg.py
+++ lcctool/plugins/raccfg.py
@@ -1,15 +1,41 @@
+# vim:expandtab:autoindent:tabstop=4:shiftwidth=4:filetype=python:tw=0
+# Copyright (c) 2011, Dell Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#    * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Dell, Inc. nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL Dell, Inc. BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import pkg_resources
 import ConfigParser
 import fnmatch
 
-import lctool
+import lcctool
 from stdcli.trace_decorator import traceLog, getLog
 from stdcli.plugin import Plugin
 
 moduleVerboseLog = getLog(prefix="verbose.")
 moduleVerboseLog.debug("initializing plugin module: %s" % __name__)
 
-_ = lctool._
+_ = lcctool._
 
 class SampleTestRacCfg(Plugin):
     @traceLog()
@@ -30,7 +56,7 @@ class RacCfg(Plugin):
         moduleVerboseLog.debug("initializing plugin: %s" % self.__class__.__name__)
         ctx.raccfg = self
         self.ctx = ctx
-        configfile = pkg_resources.resource_filename(pkg_resources.Requirement.parse("lctool"),"pkg/rachosts.ini")
+        configfile = pkg_resources.resource_filename(pkg_resources.Requirement.parse("lcctool"),"pkg/rachosts.ini")
 
         # --rac-host=HOST  --rac-user=USER  --rac-pass=PASS
         # --rac-uri=rac://USER:PASS@HOST/  --rac-uri=rac://USER2:PASS2@HOST2/ ...
