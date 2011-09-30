035214668542016f352aad4e18bdf544dc4769ea
Michael E Brown <michael_e_brown@dell.com>
2011-09-30 13:43:36 -0500

    add initial test

--- test/data/r710/get.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-wsman enumerate -P 443 -V -v -c dummy.cert -j utf-8 -y basic -o -m 512 -h 10.208.46.138 -u root -p calvin http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/root/dcim/DCIM_BIOSEnumeration > wsman1.txt
-wsman enumerate -P 443 -V -v -c dummy.cert -j utf-8 -y basic -o -m 512 -h 10.208.46.138 -u root -p calvin http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/root/dcim/DCIM_BIOSString > wsman2.txt
-wsman enumerate -P 443 -V -v -c dummy.cert -j utf-8 -y basic -o -m 512 -h 10.208.46.138 -u root -p calvin http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/root/dcim/DCIM_BIOSinteger > wsman3.txt
-
--- test/testPycompat.py
+++ test/testPycompat.py
@@ -29,16 +29,17 @@
 
 from __future__ import generators
 
-import sys
 import os
+import sys
 import unittest
+import cStringIO
+import ConfigParser
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        if globals().get('lcctool'): del(lcctool)
-        for k in sys.modules.keys():
-            if k.startswith("lcctool"):
-                del(sys.modules[k])
+        import lcctool, lcctool.wsman
+        lcctool.wsman.unit_test_mode = 1
+        lcctool.wsman.test_data_dir = os.path.join(os.path.dirname(__file__), "data")
 
     def tearDown(self):
         if globals().get('lcctool'): del(lcctool)
@@ -46,8 +47,226 @@ class TestCase(unittest.TestCase):
             if k.startswith("lcctool"):
                 del(sys.modules[k])
 
-    def testSAMPLE(self):
-        print 'hello world'
+    def testNic(self):
+        import lcctool, lcctool.wsman
+        host = {'host': 'testhost'}
+        ini = ConfigParser.ConfigParser()
+        ini.optionxform = str # need to be case sensitive
+        lcctool.wsman.stuff_xml_into_ini(host, ini, "nic")
+
+        good_ini = ConfigParser.ConfigParser()
+        good_ini.optionxform = str # need to be case sensitive
+        fh = cStringIO.StringIO(nic_ini_testdata)
+        good_ini.readfp(fh)
+        fh.close()
+        for sec in good_ini.sections():
+            for opt in good_ini.options(sec):
+                print "check [%s] %s = %s" % (sec, opt, good_ini.get(sec, opt))
+                self.assertEquals(good_ini.get(sec, opt), ini.get(sec, opt))
+
+
+## test data
+nic_ini_testdata = """
+[NIC.Embedded.4-1]
+ChipMdl = BCM5709 C0
+MacAddr = 00:24:E8:67:99:1B
+VirtMacAddr = 00:24:E8:67:99:1B
+IscsiMacAddr = 00:24:E8:67:99:1C
+VirtIscsiMacAddr = 00:24:E8:67:99:1C
+DhcpVendId = BRCM ISAN
+IscsiInitiatorIpAddr = ::
+IscsiInitiatorSubnetPrefix = 0
+IscsiInitiatorGateway = ::
+IscsiInitiatorPrimDns = ::
+IscsiInitiatorSecDns = ::
+IscsiInitiatorName = iqn.1995-05.com.broadcom.iscsiboot
+IscsiInitiatorChapId = 
+IscsiInitiatorChapPwd = 
+FirstTgtIpAddress = ::
+FirstTgtIscsiName = 
+FirstTgtChapId = 
+FirstTgtChapPwd = 
+SecondTgtIpAddress = ::
+SecondTgtIscsiName = 
+SecondTgtChapId = 
+SecondTgtChapPwd = 
+SecondaryDeviceMacAddr = 00:00:00:00:00:00
+BlnkLeds = 0
+LnkUpDelayTime = 0
+LunBusyRetryCnt = 0
+FirstTgtTcpPort = 3260
+FirstTgtBootLun = 0
+SecondTgtTcpPort = 3260
+SecondTgtBootLun = 0
+VLanId = 0
+TcpIpViaDHCP = Enabled
+IscsiViaDHCP = Enabled
+ChapAuthEnable = Disabled
+IscsiTgtBoot = Enabled
+TcpTimestmp = Disabled
+FirstHddTarget = Disabled
+IpVer = IPv6
+WinHbaBootMode = Disabled
+ConnectFirstTgt = Disabled
+ConnectSecondTgt = Disabled
+LegacyBootProto = PXE
+LnkSpeed = AutoNeg
+WakeOnLan = Disabled
+VLanMode = Disabled
+BootRetryCnt = No Retry
+UseIndTgtPortal = Disabled
+UseIndTgtName = Disabled
+
+[NIC.Embedded.3-1]
+ChipMdl = BCM5709 C0
+MacAddr = 00:24:E8:67:99:19
+VirtMacAddr = 00:24:E8:67:99:19
+IscsiMacAddr = 00:24:E8:67:99:1A
+VirtIscsiMacAddr = 00:24:E8:67:99:1A
+DhcpVendId = BRCM ISAN
+IscsiInitiatorIpAddr = 0.0.0.0
+IscsiInitiatorSubnet = 0.0.0.0
+IscsiInitiatorGateway = 0.0.0.0
+IscsiInitiatorPrimDns = 0.0.0.0
+IscsiInitiatorSecDns = 0.0.0.0
+IscsiInitiatorName = iqn.1995-05.com.broadcom.iscsiboot
+IscsiInitiatorChapId = 
+IscsiInitiatorChapPwd = 
+FirstTgtIpAddress = 0.0.0.0
+FirstTgtIscsiName = 
+FirstTgtChapId = 
+FirstTgtChapPwd = 
+SecondTgtIpAddress = 0.0.0.0
+SecondTgtIscsiName = 
+SecondTgtChapId = 
+SecondTgtChapPwd = 
+SecondaryDeviceMacAddr = 00:00:00:00:00:00
+BlnkLeds = 0
+LnkUpDelayTime = 0
+LunBusyRetryCnt = 0
+FirstTgtTcpPort = 3260
+FirstTgtBootLun = 0
+SecondTgtTcpPort = 3260
+SecondTgtBootLun = 0
+VLanId = 0
+TcpIpViaDHCP = Enabled
+IscsiViaDHCP = Enabled
+ChapAuthEnable = Disabled
+IscsiTgtBoot = Enabled
+TcpTimestmp = Disabled
+FirstHddTarget = Disabled
+IpVer = IPv4
+WinHbaBootMode = Disabled
+ConnectFirstTgt = Disabled
+ConnectSecondTgt = Disabled
+LegacyBootProto = PXE
+LnkSpeed = AutoNeg
+WakeOnLan = Disabled
+VLanMode = Disabled
+BootRetryCnt = No Retry
+UseIndTgtPortal = Disabled
+UseIndTgtName = Disabled
+
+[NIC.Embedded.2-1]
+ChipMdl = BCM5709 C0
+MacAddr = 00:24:E8:67:99:17
+VirtMacAddr = 00:24:E8:67:99:17
+IscsiMacAddr = 00:24:E8:67:99:18
+VirtIscsiMacAddr = 00:24:E8:67:99:18
+DhcpVendId = BRCM ISAN
+IscsiInitiatorIpAddr = 172.17.11.4
+IscsiInitiatorSubnet = 255.255.0.0
+IscsiInitiatorGateway = 172.17.1.150
+IscsiInitiatorPrimDns = 172.17.1.150
+IscsiInitiatorSecDns = 0.0.0.0
+IscsiInitiatorName = iqn.1995-05.com.broadcom.iscsiboot:r710:lom2
+IscsiInitiatorChapId = 
+IscsiInitiatorChapPwd = 
+FirstTgtIpAddress = 172.17.14.81
+FirstTgtIscsiName = iqn.1984-05.com.dell:powervault.md3200i.60026b9000348f3c000000004c99cb9d
+FirstTgtChapId = 
+FirstTgtChapPwd = 
+SecondTgtIpAddress = 172.17.14.80
+SecondTgtIscsiName = iqn.1984-05.com.dell:powervault.md3200i.60026b9000348f3c000000004c99cb9d
+SecondTgtChapId = 
+SecondTgtChapPwd = 
+SecondaryDeviceMacAddr = 00:00:00:00:00:00
+BlnkLeds = 0
+LnkUpDelayTime = 0
+LunBusyRetryCnt = 0
+FirstTgtTcpPort = 3260
+FirstTgtBootLun = 0
+SecondTgtTcpPort = 3260
+SecondTgtBootLun = 0
+VLanId = 0
+TcpIpViaDHCP = Disabled
+IscsiViaDHCP = Disabled
+ChapAuthEnable = Disabled
+IscsiTgtBoot = Disabled
+TcpTimestmp = Disabled
+FirstHddTarget = Enabled
+IpVer = IPv4
+WinHbaBootMode = Disabled
+ConnectFirstTgt = Enabled
+ConnectSecondTgt = Enabled
+LegacyBootProto = iSCSI
+LnkSpeed = AutoNeg
+WakeOnLan = Disabled
+VLanMode = Disabled
+BootRetryCnt = No Retry
+UseIndTgtPortal = Disabled
+UseIndTgtName = Disabled
+
+[NIC.Embedded.1-1]
+ChipMdl = BCM5709 C0
+MacAddr = 00:24:E8:67:99:15
+VirtMacAddr = 00:24:E8:67:99:15
+IscsiMacAddr = 00:24:E8:67:99:16
+VirtIscsiMacAddr = 00:24:E8:67:99:16
+DhcpVendId = BRCM ISAN
+IscsiInitiatorIpAddr = 0.0.0.0
+IscsiInitiatorSubnet = 0.0.0.0
+IscsiInitiatorGateway = 0.0.0.0
+IscsiInitiatorPrimDns = 0.0.0.0
+IscsiInitiatorSecDns = 0.0.0.0
+IscsiInitiatorName = 
+IscsiInitiatorChapId = 
+IscsiInitiatorChapPwd = 
+FirstTgtIpAddress = 0.0.0.0
+FirstTgtIscsiName = 
+FirstTgtChapId = 
+FirstTgtChapPwd = 
+SecondTgtIpAddress = 0.0.0.0
+SecondTgtIscsiName = 
+SecondTgtChapId = 
+SecondTgtChapPwd = 
+SecondaryDeviceMacAddr = 00:00:00:00:00:00
+BlnkLeds = 0
+LnkUpDelayTime = 0
+LunBusyRetryCnt = 0
+FirstTgtTcpPort = 3260
+FirstTgtBootLun = 0
+SecondTgtTcpPort = 3260
+SecondTgtBootLun = 0
+VLanId = 0
+TcpIpViaDHCP = Enabled
+IscsiViaDHCP = Enabled
+ChapAuthEnable = Disabled
+IscsiTgtBoot = Disabled
+TcpTimestmp = Disabled
+FirstHddTarget = Disabled
+IpVer = IPv4
+WinHbaBootMode = Disabled
+ConnectFirstTgt = Disabled
+ConnectSecondTgt = Disabled
+LegacyBootProto = PXE
+LnkSpeed = AutoNeg
+WakeOnLan = Disabled
+VLanMode = Disabled
+BootRetryCnt = No Retry
+UseIndTgtPortal = Disabled
+UseIndTgtName = Disabled
+"""
 
 
 if __name__ == "__main__":
